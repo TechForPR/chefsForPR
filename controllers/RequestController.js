b7180caa7ff1dacd38e6e0a5ee8d6c1156c49c80
David Silva <david.silva@dokbid.com>
2017-10-14 14:58:06 -0400

    created endpoint /api/request/:shortId to find food requests by shortid

--- controllers/RequestController.js
+++ controllers/RequestController.js
@@ -12,6 +12,22 @@ function create(req, res) {
     });
 }
 
+function getByShortId(req, res) {
+    Request.findOne({ shortId: req.params.shortId }).then(doc => {
+        if (!doc) {
+            res.status(404).send({ error: 404, message: `Request with id ${req.params.shortId} not found`});
+            return;
+        }
+        res.status(200).send( { doc });
+    }).catch((err) => {
+        res.status(500).send(Object.assign(
+            { error: 500, message: 'Server error'},
+            err,
+        ));
+    });
+}
+
 module.exports = {
     create,
+    getByShortId,
 }
