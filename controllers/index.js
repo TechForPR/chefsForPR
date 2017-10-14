80f15e3d313fbf76e549f2635e8896d2ce2499ed
David Silva <david.silva@dokbid.com>
2017-10-14 14:45:31 -0400

    added an endpoint to create a food request

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const routes = require('express').Router();
+const RequestControlller = require('./RequestController');
 
 routes.get('/', function (req, res) {
     res.status(200).send({message: 'Welcome to the API!'});
 });
+
+// Food Requests
+routes.post('/request', RequestControlller.create);
 
 routes.all('*', function (req, res) {
     res.status(404).send({error: 404, message: 'invalid endpoint'});
