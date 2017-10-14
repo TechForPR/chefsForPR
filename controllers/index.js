8abb105729f73aea076e2dd724aa7979bb8fcacf
David Silva <david.silva@dokbid.com>
2017-10-14 14:58:06 -0400

    created endpoint /api/request/:shortId to find food requests by shortid

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,7 @@ routes.get('/', function (req, res) {
 
 // Food Requests
 routes.post('/request', RequestControlller.create);
+routes.get('/request/:shortId', RequestControlller.getByShortId);
 
 routes.all('*', function (req, res) {
     res.status(404).send({error: 404, message: 'invalid endpoint'});
