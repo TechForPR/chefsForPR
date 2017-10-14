88198e438d479ce1b921e92d8ed5ef71073d9a1e
David Silva <david.silva@dokbid.com>
2017-10-14 14:03:44 -0400

    created basic tests for Request Model

--- test/index.js
+++ test/index.js
@@ -1,1 +1,2 @@
 require('./eslint');
+require('./models');
---
+++ test/models.js
@@ -0,0 +1,37 @@
+const Request = require('../models/Request');
+
+const validRequest = {
+    name: 'David Silva',
+    agency: 'Tech For PR',
+    email: 'techforpr@example.com',
+    phone: 'xxx-xxx-xxxx',
+    address: '123 fake st, apt 2',
+    zipcode: '11001',
+    questions: {
+        amountOfPeople: 10,
+    },
+    needs: {
+        breakfast: 10,
+        lunch: 10,
+        dinner: 10,
+    },
+};
+
+describe("Food Request Model", function () {
+    it("Creates a Food Request if valid", function (done) {
+        const request = new Request(validRequest);
+        request.save().then(function () {
+            done();
+        }).catch(function (err) {
+            throw err;
+        });
+    });
+
+    it("Fails to create a Food Request if validator fails", function (done) {
+        const request = new Request(Object.assign({}, validRequest, { name: undefined }));
+        request.save().then(function () {
+        }).catch(function () {
+            done();
+        });
+    });
+});
