b767a26eba0438b243d48d9a4046e33a540e88bd
David Silva <david.silva@dokbid.com>
2017-10-14 17:06:08 -0400

    created a form to request make a new food request in spanish, not functional

---
+++ helpers/formsHelpers.js
@@ -0,0 +1,17 @@
+module.exports = {
+    bootstrapFields: function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var label = object.labelHTML(name);
+    // var description = object.description ? `<p>${object.description}</p>` : '';
+    var error = object.error ? '<div class="alert alert-error help-block">' + object.error + '</div>' : '';
+
+    var validationclass = object.value && !object.error ? 'has-success' : '';
+    validationclass = object.error ? 'has-error' : validationclass;
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group ' + validationclass + '">' + label + widget + error + '</div>';
+}};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,42 @@
 const express = require('express');
+const Request = require('../models/Request');
+const boostrapFields = require('../helpers/formsHelpers').bootstrapFields;
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Chefs For Puerto Rico 🇵🇷' });
+});
+
+router.get('/request/new', function (req, res) {
+  const form = Request.createForm({
+    name: '*Su Nombre:',
+    agency: '*Nombre la agencia:',
+    email: 'Su Email, lo podriamos usar para confirmar la solicitud y coordinar la entrega',
+    phone: 'Su Telefono, lo podriamos usar para confirmar la solicitud y coordinar la entrega',
+    twitter: 'Link a su perfil Twitter',
+    facebook: 'Link a su perfil de Facebook',
+    address: '*Dirección donde realizar la entrega',
+    zipcode: 'Zipcode',
+    'amountOfPeople': '*Cantidad de personas',
+    'amountOfDays': 'Para cuantos días desea hacer la solicitud',
+    'receivingFoodAlready': 'Está recibiendo comida actualmente?',
+    'receivingFoodAlreadyDetails': 'Si está recibiendo comida, cuentenemos más acerca de lo que está recibiendo.',
+    'currentlyHaveFoodFor': 'Para cuantos más diás tiene comida?',
+    'currentlyHaveFoodForDetails': 'Cuentemos más...',
+    'breakfast': '*Cuantos desayunos necesita?',
+    'lunch': '*Cuantos almuerzos necesita?',
+    'dinner': '*Cuantas cenas necesita?',
+    'dietaryRestrictions': 'Tiene alguna restricción alimenticia?',
+    'needBy': 'En que fecha espera la comida',
+  });
+  res.render('request/new', {
+    title: 'Solicitud de Comida', form: form.toHTML(boostrapFields),
+    message: `Por favor complete la información con la mayor cantidad de detalles posible, esto nos ayuda a verificar la solicitud y procesarla lo m&atilde;s pronto que podamos.<br>
+      Los campos con *astericos son obligatorios.`,
+    submitText: 'Enviar',
+  });
 });
 
 module.exports = router;
