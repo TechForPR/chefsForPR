2496ef6b10e9867dd2fb2b10745efe6c547bbdae
David Silva <david.silva@dokbid.com>
2017-10-14 14:25:18 -0400

    Explicitly downloading and running mongodb for tests

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-mongoose.connect('mongodb://localhost:27017/chefsForPR');
+mongoose.connect('mongodb://127.0.0.1:27017/chefsForPR');
 
 module.exports = mongoose;
