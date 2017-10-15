90b89b0202725d0f413319cab505c1fd11311385
Romy Maxwell <romy.maxwell@gmail.com>
2017-10-14 22:52:35 -0400

    Adding basic english/spanish functionality
     -- Not perfect i18n, but will get the job done
     -- Schema hints are still in english

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -46,8 +46,9 @@ $(document).ready(function () {
         var form = $(this);
         var button = $(this).find('button');
         var alert =  $(this).find('.alert');
+        var language = form.find('#id_language').val();
         button.attr('disabled', true);
-        alert.removeClass('hidden alert-success alert-danger').addClass('alert-info').html('Enviando / Sending...');
+        alert.removeClass('hidden alert-success alert-danger').addClass('alert-info').html(language == 'spanish' ? 'Enviando...' : 'Sending...');
         $.ajax({
             type: 'POST',
             url: '/api/request',
@@ -55,8 +56,13 @@ $(document).ready(function () {
             dataType: 'json',
             data: JSON.stringify({ data: formObject($(this)) }),
         }).then(function (response) {
-            alert.removeClass('hidden alert-danger alert-info').addClass('alert-success').html('<strong>Se ha guardado su solicitud, pronto le responderemos / Your request was recorded, we will respond soon</strong>');
-            alert.after('<h2><small>Su codigo de confirmación es / Your confirmation code is: </small><br>' + response.doc.shortId + '</h2>');
+            if(language == 'spanish') {
+                alert.removeClass('hidden alert-danger alert-info').addClass('alert-success').html('<strong>Se ha guardado su solicitud, pronto le responderemos.</strong>');
+                alert.after('<h2><small>Su codigo de confirmación es: </small><br>' + response.doc.shortId + '</h2>');
+            } else {
+                alert.removeClass('hidden alert-danger alert-info').addClass('alert-success').html('<strong>Your request was recorded, we will respond soon.</strong>');
+                alert.after('<h2><small>Your confirmation code is: </small><br>' + response.doc.shortId + '</h2>');
+            }
             form.find('.form-group, .btn').addClass('hidden');
             window.location.replace('/request/' + response.doc.shortId);
             //TODO: redirect to details page when available
@@ -66,7 +72,7 @@ $(document).ready(function () {
             $('.has-error .text-danger').remove();
             $('.has-error').removeClass('has-error');
             if (response.status === 400) {
-                alert.html('Por favor complete la informacíon que falta / Please complete the missing data');
+                alert.html(language == 'spanish' ? 'Por favor complete la informacíon que falta' : 'Please complete the missing data');
                 // validation failed for the Request
                 // display the errors in the form
                 if (response.responseJSON && response.responseJSON.errors) {
@@ -81,7 +87,7 @@ $(document).ready(function () {
                     }
                 }
             } else {
-                alert.html('Hubo un error al solicitar, por favor intente mas tarde / There was an error saving, please try again later.');
+                alert.html(language == 'spanish' ? 'Hubo un error al solicitar, por favor intente mas tarde.' : 'There was an error saving, please try again later.');
             }
         });
     });
