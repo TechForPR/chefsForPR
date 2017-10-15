c3d2bb78d8f3977802a5c5420796241a4d59ce25
David Silva <david.silva@dokbid.com>
2017-10-15 15:45:42 -0400

    created api enpodints for delivery create and findByShortId

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const RequestControlller = require('./RequestController');
+const DeliveryControlller = require('./DeliveryController');
 
 routes.get('/', function (req, res) {
     res.status(200).send({message: 'Welcome to the API!'});
@@ -9,6 +10,9 @@ routes.get('/', function (req, res) {
 routes.post('/request', RequestControlller.create);
 routes.get('/request/:shortId', RequestControlller.getByShortId);
 routes.get('/requests', RequestControlller.getByQueryParams);
+
+// Delivery reports
+routes.post('/delivery', DeliveryControlller.create);
 
 routes.all('*', function (req, res) {
     res.status(404).send({error: 404, message: 'invalid endpoint'});
