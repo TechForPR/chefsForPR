09a430bf3eb0daa4a86d027333498e79cf9933da
David Silva <david.silva@dokbid.com>
2017-10-16 12:51:49 -0400

    created a form to track food deliveries, refactored some work in routes

--- config/constants.js
+++ config/constants.js
@@ -15,8 +15,56 @@ const deliveryStatuses = [
 
 const longDateFormat = 'MMMM DD YYYY hh:mm A';
 
+const requestLabelsSpanish = {
+    name: '*Su Nombre:',
+    agency: 'Nombre la agencia:',
+    email: '*Su Email, lo podriamos usar para confirmar la solicitud y coordinar la entrega',
+    phone: '*Su Telefono, lo podriamos usar para confirmar la solicitud y coordinar la entrega',
+    twitter: 'Link a su perfil Twitter',
+    facebook: 'Link a su perfil de Facebook',
+    address: '*Dirección donde realizar la entrega',
+    city: '*Ciudad',
+    zipcode: '*Zipcode',
+    'amountOfPeople': '*Cantidad de personas',
+    'amountOfDays': 'Para cuantos días desea hacer la solicitud',
+    'receivingFoodAlready': 'Está recibiendo comida actualmente?',
+    'receivingFoodAlreadyDetails': 'Si está recibiendo comida, cuentenemos más acerca de lo que está recibiendo.',
+    'currentlyHaveFoodFor': 'Para cuantos más diás tiene comida?',
+    'currentlyHaveFoodForDetails': 'Cuentemos más...',
+    'breakfast': '*Cuantos desayunos necesita?',
+    'lunch': '*Cuantos almuerzos necesita?',
+    'dinner': '*Cuantas cenas necesita?',
+    'dietaryRestrictions': 'Tiene alguna restricción alimenticia?',
+    'needBy': 'En que fecha espera la comida',
+  };
+
+const requestLabelsEnglish = {
+    name: '*Your Name:',
+    agency: 'Agency Name:',
+    email: '*Your Email, used to confirm the request and coordinate the delivery',
+    phone: '*Your Phone, used to confirm the request and coordinate the delivery',
+    twitter: 'Link to your Twitter profile',
+    facebook: 'Link to your Facebook profile',
+    address: '*Address for delivery',
+    city: '*City',
+    zipcode: '*Zipcode',
+    'amountOfPeople': '*Amount of people',
+    'amountOfDays': 'For how many days of delivery is this request?',
+    'receivingFoodAlready': 'Are you currently receiving food?',
+    'receivingFoodAlreadyDetails': 'If currently receiving food, tell us more about what you are receiving',
+    'currentlyHaveFoodFor': 'For how many more days do you have food?',
+    'currentlyHaveFoodForDetails': 'Tell us more...',
+    'breakfast': '*How many breakfasts do you need?',
+    'lunch': '*How many lunches do you need?',
+    'dinner': '*How many dinners do you need?',
+    'dietaryRestrictions': 'Do you have any dietary restrictions?',
+    'needBy': 'By what date do you need the food?',
+  };
+
 module.exports = {
     requestStatuses,
     longDateFormat,
     deliveryStatuses,
+    requestLabelsSpanish,
+    requestLabelsEnglish,
 }
--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -44,61 +44,67 @@ if (!Array.isArray) {
     };
   }
 
+var submitNewForm = function (dest) {
+    var form = $(this);
+    var button = $(this).find('button');
+    var alert =  $(this).find('.alert');
+    var language = form.find('#id_language').val() || 'english';
+    button.attr('disabled', true);
+    alert.removeClass('hidden alert-success alert-danger').addClass('alert-info').html(language == 'spanish' ? 'Enviando...' : 'Sending...');
+    $.ajax({
+        type: 'POST',
+        url: '/api/' + dest,
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: JSON.stringify({ data: formObject($(this)) }),
+    }).then(function (response) {
+        if(language == 'spanish') {
+            alert.removeClass('hidden alert-danger alert-info').addClass('alert-success').html('<strong>Se ha guardado su solicitud, pronto le responderemos.</strong>');
+            alert.after('<h2><small>Su codigo de confirmación es: </small><br>' + response.doc.shortId + '</h2>');
+        } else {
+            alert.removeClass('hidden alert-danger alert-info').addClass('alert-success').html('<strong>Your request was recorded, we will respond soon.</strong>');
+            alert.after('<h2><small>Your confirmation code is: </small><br>' + response.doc.shortId + '</h2>');
+        }
+        form.find('.form-group, .btn').addClass('hidden');
+        window.location.replace('/' + dest + '/' + response.doc.shortId);
+        //TODO: redirect to details page when available
+    }, function (response) {
+        button.attr('disabled', false);
+        alert.removeClass('hidden alert-info alert-success').addClass('alert-danger');
+        $('.has-error .text-danger').remove();
+        $('.has-error').removeClass('has-error');
+        if (response.status === 400) {
+            alert.html(language == 'spanish' ? 'Por favor complete la informacíon que falta' : 'Please complete the missing data');
+            // validation failed for the Request
+            // display the errors in the form
+            if (response.responseJSON && response.responseJSON.errors) {
+                var errors = response.responseJSON.errors;
+                for (var error in errors) {
+                    // TODO: abstract this somewhere else
+                    if (errors.hasOwnProperty(error)) {
+                        $('[name="'+error+'"]').parent('.form-group').addClass('has-error');
+                        $('[name="'+error+'"]').after('<p class="text-danger">' + errors[error].message + '</p>');
+                    }
+                }
+            }
+        } else {
+            alert.html(language == 'spanish' ? 'Hubo un error en la solicitud, por favor intente mas tarde.' : 'There was an error saving, please try again later.');
+        }
+    });
+}
 
-// Submit form in /request/new
+// Submit form in /request/new and /delivery/new
 $(document).ready(function () {
     $('#new-request form').on('submit', function (e) {
         e.preventDefault();
-        var form = $(this);
-        var button = $(this).find('button');
-        var alert =  $(this).find('.alert');
-        var language = form.find('#id_language').val();
-        button.attr('disabled', true);
-        alert.removeClass('hidden alert-success alert-danger').addClass('alert-info').html(language == 'spanish' ? 'Enviando...' : 'Sending...');
-        $.ajax({
-            type: 'POST',
-            url: '/api/request',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            data: JSON.stringify({ data: formObject($(this)) }),
-        }).then(function (response) {
-            if(language == 'spanish') {
-                alert.removeClass('hidden alert-danger alert-info').addClass('alert-success').html('<strong>Se ha guardado su solicitud, pronto le responderemos.</strong>');
-                alert.after('<h2><small>Su codigo de confirmación es: </small><br>' + response.doc.shortId + '</h2>');
-            } else {
-                alert.removeClass('hidden alert-danger alert-info').addClass('alert-success').html('<strong>Your request was recorded, we will respond soon.</strong>');
-                alert.after('<h2><small>Your confirmation code is: </small><br>' + response.doc.shortId + '</h2>');
-            }
-            form.find('.form-group, .btn').addClass('hidden');
-            window.location.replace('/request/' + response.doc.shortId);
-            //TODO: redirect to details page when available
-        }, function (response) {
-            button.attr('disabled', false);
-            alert.removeClass('hidden alert-info alert-success').addClass('alert-danger');
-            $('.has-error .text-danger').remove();
-            $('.has-error').removeClass('has-error');
-            if (response.status === 400) {
-                alert.html(language == 'spanish' ? 'Por favor complete la informacíon que falta' : 'Please complete the missing data');
-                // validation failed for the Request
-                // display the errors in the form
-                if (response.responseJSON && response.responseJSON.errors) {
-                    var errors = response.responseJSON.errors;
-                    console.log(errors);
-                    for (var error in errors) {
-                        // TODO: abstract this somewhere else
-                        if (errors.hasOwnProperty(error)) {
-                            $('[name="'+error+'"]').parent('.form-group').addClass('has-error');
-                            $('[name="'+error+'"]').after('<p class="text-danger">' + errors[error].message + '</p>');
-                        }
-                    }
-                }
-            } else {
-                alert.html(language == 'spanish' ? 'Hubo un error al solicitar, por favor intente mas tarde.' : 'There was an error saving, please try again later.');
-            }
-        });
+        submitNewForm.bind(this)('request');
+    });
+
+    $('#new-delivery form').on('submit', function (e) {
+        e.preventDefault();
+        submitNewForm.bind(this)('delivery');
     });
 });
-
 
 // Get list of requests in /requests
 var getRequests = function (filter) {
