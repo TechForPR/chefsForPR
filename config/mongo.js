91d85773443460242f347f2fc0154afc17c9b496
David Silva <david.silva@dokbid.com>
2017-10-27 16:34:27 -0400

    restored the name of the db

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-mongoose.connect('mongodb://127.0.0.1:27017/chefsForPRTest');
+mongoose.connect('mongodb://127.0.0.1:27017/chefsForPR');
 
 module.exports = mongoose;
